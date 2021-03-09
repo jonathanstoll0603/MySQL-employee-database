9231edd567611126384284014280c5c98ebee86b
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-08 23:46:29 -0500

    update seeds

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -31,3 +31,21 @@ INSERT INTO employee_role (title, salary, department_id)
 VALUES ('Manufacturing Manager', 120000, 4);
 INSERT INTO employee_role (title, salary, department_id)
 VALUES ('Manufacturing Technician', 50000, 4);
+
+-- seeds for employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jane', 'Doe', 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doenut', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Scotty', 'Scotch', 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Sarah', 'Haras', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Lindsey', 'Peterson', 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Bobby', 'Snow', 6, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Rick', 'Roll', 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Pete', 'Davidson', 8, 7);
