1d4cac368ceab5ad53e5a51f6bdd84f6679094bd
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-14 14:57:11 -0400

    added mysql validation and cascade commands

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,27 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role (
-    role_id INT AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(12,2) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(role_id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    role_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL(12,2) UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+    INDEX dep_id (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES employee_role(role_id),
-    FOREIGN KEY (manager_id) REFERENCES employee_role(role_id)
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
+    manager_id INT UNSIGNED,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
